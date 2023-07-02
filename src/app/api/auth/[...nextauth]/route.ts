f774283dd31cb53506048c9a017552e29ed71c1f
Losty <vinikappke@gmail.com>
2023-07-02 17:37:23 -0300

    fix nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth";
 
-export const authOptions = {
+export const options = {
   // Configure one or more authentication providers
   providers: [
     // ...add more providers here
   ],
 };
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(options);
 
 export { handler as GET, handler as POST };
